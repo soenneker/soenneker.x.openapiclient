c7caa3cbb6ae304a26b15c8fc8ba57b8500a3152
Jake Soenneker <jake@soeneker.com>
2025-10-28 17:21:09 +0000

    Automated update

--- src/Models/ActivitySubscriptionCreateRequest_event_type.cs
+++ src/Models/ActivitySubscriptionCreateRequest_event_type.cs
@@ -36,5 +36,9 @@ namespace Soenneker.X.OpenApiClient.Models
         #pragma warning disable CS1591
         ProfileVerifiedBadgeUpdate,
         #pragma warning restore CS1591
+        [EnumMember(Value = "ProfileHandleUpdate")]
+        #pragma warning disable CS1591
+        ProfileHandleUpdate,
+        #pragma warning restore CS1591
     }
 }
