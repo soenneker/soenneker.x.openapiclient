97d366d979f4982c628d3201eb7b548fb94de6d8
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-08-01 18:03:33 +0000

    Automated update

--- src/Soenneker.X.OpenApiClient/Models/GetOpenApiSpec200ResponseSchema.cs
+++ src/Soenneker.X.OpenApiClient/Models/GetOpenApiSpec200ResponseSchema.cs
@@ -9,27 +9,27 @@ namespace Soenneker.X.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class GetOpenApiSpec200ResponseResponseJson : IAdditionalDataHolder, IParsable
+    public partial class GetOpenApiSpec200ResponseSchema : IAdditionalDataHolder, IParsable
     #pragma warning restore CS1591
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
         /// <summary>
-        /// Instantiates a new <see cref="global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseResponseJson"/> and sets the default values.
+        /// Instantiates a new <see cref="global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseSchema"/> and sets the default values.
         /// </summary>
-        public GetOpenApiSpec200ResponseResponseJson()
+        public GetOpenApiSpec200ResponseSchema()
         {
             AdditionalData = new Dictionary<string, object>();
         }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseResponseJson"/></returns>
+        /// <returns>A <see cref="global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseSchema"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseResponseJson CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseSchema CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseResponseJson();
+            return new global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseSchema();
         }
         /// <summary>
         /// The deserialization information for the current model
--- src/Soenneker.X.OpenApiClient/Two/OpenapiJson/OpenapiJsonRequestBuilder.cs
+++ src/Soenneker.X.OpenApiClient/Two/OpenapiJson/OpenapiJsonRequestBuilder.cs
@@ -36,20 +36,20 @@ namespace Soenneker.X.OpenApiClient.Two.OpenapiJson
         /// <summary>
         /// Retrieves the full OpenAPI Specification in JSON format. (See https://github.com/OAI/OpenAPI-Specification/blob/master/README.md)
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseResponseJson"/></returns>
+        /// <returns>A <see cref="global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseSchema"/></returns>
         /// <param name="cancellationToken">Cancellation token to use when cancelling requests</param>
         /// <param name="requestConfiguration">Configuration for the request such as headers, query parameters, and middleware options.</param>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public async Task<global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseResponseJson?> GetAsync(Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = default, CancellationToken cancellationToken = default)
+        public async Task<global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseSchema?> GetAsync(Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = default, CancellationToken cancellationToken = default)
         {
 #nullable restore
 #else
-        public async Task<global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseResponseJson> GetAsync(Action<RequestConfiguration<DefaultQueryParameters>> requestConfiguration = default, CancellationToken cancellationToken = default)
+        public async Task<global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseSchema> GetAsync(Action<RequestConfiguration<DefaultQueryParameters>> requestConfiguration = default, CancellationToken cancellationToken = default)
         {
 #endif
             var requestInfo = ToGetRequestInformation(requestConfiguration);
-            return await RequestAdapter.SendAsync<global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseResponseJson>(requestInfo, global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseResponseJson.CreateFromDiscriminatorValue, default, cancellationToken).ConfigureAwait(false);
+            return await RequestAdapter.SendAsync<global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseSchema>(requestInfo, global::Soenneker.X.OpenApiClient.Models.GetOpenApiSpec200ResponseSchema.CreateFromDiscriminatorValue, default, cancellationToken).ConfigureAwait(false);
         }
         /// <summary>
         /// Retrieves the full OpenAPI Specification in JSON format. (See https://github.com/OAI/OpenAPI-Specification/blob/master/README.md)
